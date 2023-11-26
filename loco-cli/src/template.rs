86a2b87d9b92b13ca31292b838496ac9f8269829
Elad Kaplan <kaplan.elad@gmail.com>
2023-11-26 15:17:53 +0200

    adding env vars to loco cli

--- loco-cli/src/template.rs
+++ loco-cli/src/template.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use dialoguer::{theme::ColorfulTheme, Input, Select};
 use regex::Regex;
 
@@ -18,10 +20,16 @@ pub fn prompt_app() -> eyre::Result<String> {
         .with_prompt("❯ App name?")
         .default("myapp".to_string())
         .interact_text()?;
-    if !Regex::new("^[a-zA-Z0-9_]+$")?.is_match(&app) {
+    validate_app_name(&app)?;
+    Ok(app)
+}
+
+pub fn validate_app_name(app: &str) -> eyre::Result<()> {
+    if !Regex::new("^[a-zA-Z0-9_]+$")?.is_match(app) {
         eyre::bail!("app name is invalid, illegal characters. keep names simple: myapp or my_app");
     }
-    Ok(app)
+
+    Ok(())
 }
 
 pub fn prompt_selection() -> eyre::Result<String> {
@@ -33,4 +41,42 @@ pub fn prompt_selection() -> eyre::Result<String> {
 
     println!("{}", &OPTIONS[selection].1);
     Ok(OPTIONS[selection].2.to_string())
+}
+
+pub fn get_template_url_by_name(name: &str) -> eyre::Result<String> {
+    let options_map: HashMap<_, _> = OPTIONS
+        .iter()
+        .map(|&(key, _, value)| (key, value))
+        .collect();
+
+    let val = options_map
+        .get(name)
+        .ok_or(eyre::eyre!("template not found"))?;
+
+    Ok((*val).to_string())
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn can_get_template_url_by_name() {
+        assert_eq!(
+            get_template_url_by_name("saas").ok(),
+            Some(SAAS_STARTER.to_string())
+        );
+        assert_eq!(
+            get_template_url_by_name("stateless").ok(),
+            Some(STATELESS_STARTER.to_string())
+        );
+        assert!(get_template_url_by_name("invalid").is_err(),);
+    }
+
+    #[test]
+    fn can_validate_app_name() {
+        assert!(validate_app_name("myapp").is_ok());
+        assert!(validate_app_name("myapp_webserver").is_ok());
+        assert!(validate_app_name("myapp-webserver").is_err());
+    }
 }
