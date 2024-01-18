6f6b52000959b331c6e73fdf2bd0496d0150ecdd
Dotan J. Nahum <jondotan@gmail.com>
2024-01-18 17:03:40 +0200

    E2e db (#371)
    ci: add tests

--- examples/demo/tests/cli_tests.rs
+++ examples/demo/tests/cli_tests.rs
@@ -2,8 +2,6 @@ use std::env;
 
 #[test]
 fn cli_tests() {
-    if env::var("LOCO_CI_MODE").is_ok() {
-        let t = trycmd::TestCases::new();
-        t.case("tests/cmd/*.trycmd");
-    }
+    let t = trycmd::TestCases::new();
+    t.case("tests/cmd/*.trycmd");
 }
