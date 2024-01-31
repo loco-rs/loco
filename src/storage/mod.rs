cac7734ee6e2203f7bdc63ab84be810605dee3c8
Naiker <yinho999@gmail.com>
2024-01-31 09:16:32 +0000

    optimizing code for storage by using generics

--- src/storage/mod.rs
+++ src/storage/mod.rs
@@ -10,57 +10,63 @@ use object_store::{ObjectStore, PutResult};
 use object_store::path::Path;
 use crate::{config, Error};
 use super::Result;
-
+use object_store::{ Result as ObjectStoreResult};
+use bytes::Bytes;
 /// A [`Storage`] struct representing the storage options available to the application.
 #[derive(Default, Clone)]
 pub struct Storage {
-    pub local: Option<LocalStorage>,
+    pub local: Option<GenericStorage<LocalFileSystem>>,
     // #[cfg(feature = "store-aws")]
-    pub aws_s3: Option<AwsS3Storage>,
+    pub aws_s3: Option<GenericStorage<AmazonS3>>,
     // #[cfg(feature = "store-gcs")]
-    pub gcs: Option<GcsStorage>,
+    pub gcs: Option<GenericStorage<GoogleCloudStorage>>,
     // #[cfg(feature = "store-azure")]
-    pub azure: Option<AzureStorage>,
+    pub azure: Option<GenericStorage<MicrosoftAzure>>,
+}
+
+
+#[async_trait]
+pub trait StoragePut {
+    async fn put(&self, file: Bytes, custom_path: Option<&str>) -> ObjectStoreResult<PutResult>;
+}
+
+pub struct GenericStorage<T: ObjectStore + Send + Sync + 'static>(Arc<T>, object_store::path::Path);
+
+impl<T> GenericStorage<T>
+where
+    T: ObjectStore + Send + Sync + 'static,
+{
+    pub fn new(storage: T, path: object_store::path::Path) -> Self {
+        GenericStorage(Arc::new(storage), path)
+    }
+}
+impl<T> Clone for GenericStorage<T>
+where
+    T: ObjectStore + Send + Sync + 'static,
+{
+    fn clone(&self) -> Self {
+        GenericStorage(Arc::clone(&self.0), self.1.clone())
+    }
 }
 
 #[async_trait]
-trait StoragePut {
-    fn get_storage_path(&self) -> (Box<Arc<dyn ObjectStore>>, object_store::path::Path);
-    async fn put(&self, file: bytes::Bytes, custom_path: Option<&str>) -> object_store::Result<PutResult> {
-        let (storage, path) = self.get_storage_path();
-        let path = custom_path.map(Path::from).unwrap_or(path);
-        storage.put(&path, file).await
+impl<T> StoragePut for GenericStorage<T>
+where
+    T: ObjectStore + Send + Sync + 'static,
+{
+    async fn put(&self, file: Bytes, custom_path: Option<&str>) -> ObjectStoreResult<PutResult> {
+        let path = custom_path.map(object_store::path::Path::from).unwrap_or_else(|| self.1.clone());
+        self.0.put(&path, file).await
     }
 }
 
-#[derive(Debug, Clone)]
-pub struct LocalStorage(Arc<LocalFileSystem>, object_store::path::Path);
-
-impl StoragePut for LocalStorage {
-    fn get_storage_path(&self) -> (Box<Arc<dyn ObjectStore>>, object_store::path::Path) {
-        let store: Box<Arc<dyn ObjectStore>> = Box::new(self.0.clone());
-        (store, self.1.clone())
-    }
-}
-
-// #[cfg(feature = "store-aws")]
-#[derive(Clone)]
-pub struct AwsS3Storage(Arc<AmazonS3>, object_store::path::Path);
-
-// #[cfg(feature = "store-gcs")]
-#[derive(Clone)]
-pub struct GcsStorage(Arc<GoogleCloudStorage>, object_store::path::Path);
-
-// #[cfg(feature = "store-azure")]
-#[derive(Clone)]
-pub struct AzureStorage(Arc<MicrosoftAzure>, object_store::path::Path);
 
 impl Storage {
     /// Creates a new [`Storage`] instance with the provided options.
     pub fn new(config: &config::Storage) -> Result<Self> {
         let mut storage = Self::default();
         if let Some(local) = &config.local {
-            let local = LocalStorage(Arc::new(LocalFileSystem::new()), object_store::path::Path::from(local.path.clone()));
+            let local = GenericStorage::new(LocalFileSystem::new(), object_store::path::Path::from(local.path.clone()));
             storage.local = Some(local);
         }
         if let Some(aws_config) = &config.amazon_s3 {
@@ -70,7 +76,7 @@ impl Storage {
                     Error::Any("Failed to create AWS S3 client".to_string().into())
                 }
             )?;
-            let aws_s3 = AwsS3Storage(Arc::new(aws), object_store::path::Path::from(aws_config.path.clone()));
+            let aws_s3 = GenericStorage::new(aws, object_store::path::Path::from(aws_config.path.clone()));
             storage.aws_s3 = Some(aws_s3);
         }
         if let Some(gcs_config) = &config.google_cloud_storage {
@@ -80,7 +86,7 @@ impl Storage {
                     Error::Any("Failed to create Google Cloud Storage client".to_string().into())
                 }
             )?;
-            let gcs = GcsStorage(Arc::new(gcs), object_store::path::Path::from(gcs_config.path.clone()));
+            let gcs = GenericStorage::new(gcs, object_store::path::Path::from(gcs_config.path.clone()));
             storage.gcs = Some(gcs);
         }
         if let Some(azure_config) = &config.azure_blob_storage {
@@ -90,9 +96,31 @@ impl Storage {
                     Error::Any("Failed to create Azure Blob Storage client".to_string().into())
                 }
             )?; // .with_blob_name(&azure_config.blob_name
-            let azure = AzureStorage(Arc::new(azure), object_store::path::Path::from(azure_config.path.clone()));
+            let azure = GenericStorage::new(azure, object_store::path::Path::from(azure_config.path.clone()));
             storage.azure = Some(azure);
         }
         Ok(storage)
     }
 }
+
+#[derive(Debug)]
+pub enum StorageError {
+    AwsClientCreationError(String),
+    GcsClientCreationError(String),
+    AzureClientCreationError(String),
+    LocalStorageError(String),
+}
+
+impl std::fmt::Display for StorageError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match *self {
+            StorageError::AwsClientCreationError(ref err) => write!(f, "AWS S3 Client Creation Error: {}", err),
+            StorageError::GcsClientCreationError(ref err) => write!(f, "Google Cloud Storage Client Creation Error: {}", err),
+            StorageError::AzureClientCreationError(ref err) => write!(f, "Azure Blob Storage Client Creation Error: {}", err),
+            StorageError::LocalStorageError(ref err) => write!(f, "Local Storage Error: {}", err),
+            // Handle other errors similarly
+        }
+    }
+}
+
+impl std::error::Error for StorageError {}
