ae70d7fd7732149dd50e666d2ca58f97b05651c8
Naiker <yinho999@gmail.com>
2024-02-10 19:11:31 +0000

    Finished testing and documentation

--- src/oauth2_store/grants/authorization_code.rs
+++ src/oauth2_store/grants/authorization_code.rs
@@ -13,11 +13,15 @@ use subtle::ConstantTimeEq;
 
 use crate::oauth2_store::error::{OAuth2ClientError, OAuth2ClientResult};
 
+/// A credentials struct that holds the OAuth2 client credentials. - For
+/// [`AuthorizationCodeClient`]
 pub struct AuthorizationCodeCredentials {
     pub client_id: String,
     pub client_secret: Option<String>,
 }
 
+/// A url config struct that holds the OAuth2 client related URLs. - For
+/// [`AuthorizationCodeClient`]
 pub struct AuthorizationCodeUrlConfig {
     pub auth_url: String,
     pub token_url: Option<String>,
@@ -26,19 +30,52 @@ pub struct AuthorizationCodeUrlConfig {
     pub scopes: Vec<String>,
 }
 
-/// A struct that holds the OAuth2 client and the profile URL. - For
-/// Authorization Code Grant
+/// [`AuthorizationCodeClient`] that acts as a client for the Authorization Code
+/// Grant flow.
 pub struct AuthorizationCodeClient {
+    /// [`BasicClient`] instance for the OAuth2 client.
     pub oauth2: BasicClient,
+    /// [`Url`] instance for the OAuth2 client's profile URL.
     pub profile_url: url::Url,
+    /// [`reqwest::Client`] instance for the OAuth2 client's HTTP client.
     pub http_client: reqwest::Client,
+    /// A flow states hashMap <CSRF Token, (PKCE Code Verifier, Created time)>
+    /// for managing the expiration of the CSRF tokens and PKCE code verifiers.
     pub flow_states: HashMap<String, (PkceCodeVerifier, Instant)>,
+    /// A vector of [`Scope`] for the getting the user's profile.
     pub scopes: Vec<Scope>,
+    /// A [`std::time::Duration`] for the OAuth2 client's CSRF token timeout
+    /// which defaults to 10 minutes (600s).
     pub csrf_token_timeout: std::time::Duration,
 }
 
 impl AuthorizationCodeClient {
-    /// Create a new instance of `OAuth2Client`.
+    /// Create a new instance of [`OAuth2Client`].
+    /// # Arguments
+    /// * `credentials` - A [`AuthorizationCodeCredentials`] struct that holds
+    ///   the OAuth2 client credentials.
+    /// * `config` - A [`AuthorizationCodeUrlConfig`] struct that holds the
+    ///   OAuth2 client related URLs.
+    /// * `timeout_seconds` - An optional timeout in seconds for the csrf token.
+    ///   Defaults to 10 minutes (600s).
+    /// # Returns
+    /// A Result with the [`AuthorizationCodeClient`] instance or an
+    /// [`OAuth2ClientError`].
+    /// # Example
+    /// ```rust
+    /// let credentials = AuthorizationCodeCredentials {
+    ///    client_id: "test_client_id".to_string(),
+    ///   client_secret: Some("test_client_secret".to_string()),
+    /// };
+    /// let config = AuthorizationCodeUrlConfig {
+    ///     auth_url: "https://accounts.google.com/o/oauth2/v2/auth".to_string(),
+    ///     token_url: Some("https://www.googleapis.com/oauth2/v3/token".to_string()),
+    ///     redirect_url: "http://localhost:8000/api/auth/google_callback".to_string(),
+    ///     profile_url: "https://openidconnect.googleapis.com/v1/userinfo".to_string(),
+    ///     scopes: vec!["https://www.googleapis.com/auth/userinfo.email".to_string()],
+    /// };
+    /// let client = AuthorizationCodeClient::new(credentials, config, None)?;
+    /// ```
     pub fn new(
         credentials: AuthorizationCodeCredentials,
         config: AuthorizationCodeUrlConfig,
@@ -70,25 +107,85 @@ impl AuthorizationCodeClient {
             csrf_token_timeout: std::time::Duration::from_secs(timeout_seconds.unwrap_or(10 * 60)),
         })
     }
+    /// Remove expired flow states within the [`AuthorizationCodeClient`].
+    /// # Example
+    /// ```rust
+    /// client.remove_expire_flow(); // Clear outdated states within client.flow_states
+    /// ```
     fn remove_expire_flow(&mut self) {
         // Remove expired tokens
         self.flow_states
             .retain(|_, (_, created_at)| created_at.elapsed() < self.csrf_token_timeout);
     }
-}
-
-fn constant_time_compare(a: &str, b: &str) -> bool {
-    // Convert the strings to bytes for comparison.
-    // Note: This assumes both slices are of the same length.
-    // You might want to handle differing lengths explicitly, depending on your
-    // security requirements.
-    a.as_bytes().ct_eq(b.as_bytes()).into()
+    /// Compare two strings in constant time to prevent timing attacks.
+    /// # Arguments
+    /// * `a` - A string to compare.
+    /// * `b` - A string to compare.
+    /// # Returns
+    /// A boolean value indicating if the strings are equal.
+    /// # Example
+    /// ```rust
+    /// AuthorizationCodeClient::constant_time_compare("test", "test"); // true
+    /// AuthorizationCodeClient::constant_time_compare("test", "test1"); // false
+    /// ```
+    fn constant_time_compare(a: &str, b: &str) -> bool {
+        // Convert the strings to bytes for comparison.
+        a.as_bytes().ct_eq(b.as_bytes()).into()
+    }
 }
 
 #[async_trait::async_trait]
 pub trait AuthorizationCodeGrantTrait: Send + Sync {
+    /// Get authorization code client
+    /// # Returns
+    /// A mutable reference to the [`AuthorizationCodeClient`] instance.
     fn get_authorization_code_client(&mut self) -> &mut AuthorizationCodeClient;
+
     /// Get authorization URL
+    /// # Returns
+    /// A tuple containing the authorization URL and the CSRF token.
+    /// [`Url`] is used to redirect the user to the OAuth2
+    /// provider's login page.
+    /// [`CsrfToken`] is used to verify the user
+    /// when they return to the application. Needs to be stored in the session
+    /// or other temporary storage.
+    /// # Example
+    /// ```rust
+    /// use oauth2::CsrfToken;
+    /// use oauth2::url::Url;
+    /// use oauth2::basic::BasicClient;
+    /// use oauth2::reqwest::async_http_client;
+    ///
+    ///  // Create a new instance of session store - from tower-sessions
+    ///  let session_store = MemoryStore::default();
+    ///  // Create a new instance of `SessionManagerLayer` with the session store for axum layer
+    ///  let session_layer = SessionManagerLayer::new(session_store)
+    ///     // This is needed because the oauth2 client callback request is coming from a different domain, but be careful with this in production since it can be a security risk.
+    ///     .with_same_site(SameSite::Lax);
+    ///  // Create a new instance of `OAuth2ClientStore` with the `AuthorizationCodeClient` instance
+    ///  let client = AuthorizationCodeClient::new();
+    ///  let mut clients = BTreeMap::new();
+    ///  clients.insert(
+    ///    "google".to_string(),
+    ///    OAuth2ClientGrantEnum::AuthorizationCode(Arc::new(Mutex::new(authorization_code_client))),
+    ///  );
+    ///  let mut client_store = OAuth2ClientStore::new(clients);
+    ///  let app = Router::new().route("/auth/google", get(get_auth_url)).layer(Extension(Arc::new(client_store))).layer(session_layer);
+    ///
+    ///  pub async fn get_auth_url(Extension(session_store): Extension<Session>, Extension(oauth_client_store): Extension<Arc<OAuth2ClientStore>>,) -> Url {
+    ///     let client = oauth_client_store.get("google").unwrap();
+    ///     // Get the authorization URL and the CSRF token
+    ///     let (auth_url, csrf_token) = client.get_authorization_url();
+    ///
+    ///     // Save the CSRF token in the session store
+    ///     session_store
+    ///         .insert("csrf_token", saved_csrf_token)
+    ///         .await
+    ///         .unwrap();
+    ///     // redirect the user to the authorization URL
+    ///     Ok(auth_url)
+    /// }
+    /// ```
     fn get_authorization_url(&mut self) -> (Url, CsrfToken) {
         let client = self.get_authorization_code_client();
         // Clear outdated flow states
@@ -114,8 +211,92 @@ pub trait AuthorizationCodeGrantTrait: Send + Sync {
             .insert(csrf_secret.clone(), (pkce_verifier, Instant::now()));
         (auth_url, csrf_token)
     }
-
-    async fn verify_user_with_code(
+    /// Verify code from the provider callback request after returns from the
+    /// OAuth2 provider's login page.
+    /// # Arguments
+    /// * `code` - A string containing the code returned from the OAuth2
+    ///   provider callback request query.
+    /// * `state` - A string containing the state returned from the OAuth2
+    ///   provider response which extracted from the provider callback request
+    ///   query.
+    /// * `csrf_token` - A string containing the CSRF token saved in the
+    ///   temporary session after the
+    ///   [`AuthorizationCodeClient::get_authorization_url`] method.
+    /// # Returns
+    /// A tuple containing the token response and the profile response.
+    /// [`BasicTokenResponse`] is the token response from the OAuth2 provider.
+    /// [`Response`] is the profile response from the OAuth2 provider which
+    /// describes the user's profile. This response json information will be
+    /// determined by [`AuthorizationCodeClient::scopes`] # Errors
+    /// An [`OAuth2ClientError::CsrfTokenError`] if the csrf token is invalid.
+    /// An [`OAuth2ClientError::BasicTokenError`] if the token
+    /// exchange fails.
+    /// An [`OAuth2ClientError::ProfileError`] if the profile request fails.
+    /// # Example
+    /// ```rust
+    /// use std::collections::BTreeMap;
+    /// use std::sync::Arc;
+    /// use std::time::Duration;
+    /// use axum::{Extension, Router};
+    /// use axum::extract::Query;
+    /// use axum::response::Redirect;
+    /// use axum::routing::get;use oauth2::CsrfToken;
+    /// use oauth2::url::Url;
+    /// use oauth2::basic::BasicClient;
+    /// use oauth2::reqwest::async_http_client;
+    /// use serde::Deserialize;
+    /// use tokio::sync::Mutex;
+    /// use super::*;
+    ///
+    ///  // Create a new instance of session store - from tower-sessions
+    ///  let session_store = MemoryStore::default();
+    ///  // Create a new instance of `SessionManagerLayer` with the session store for axum layer
+    ///  let session_layer = SessionManagerLayer::new(session_store)
+    ///     // This is needed because the oauth2 client callback request is coming from a different domain, but be careful with this in production since it can be a security risk.
+    ///     .with_same_site(SameSite::Lax);
+    ///  // Create a new instance of `OAuth2ClientStore` with the `AuthorizationCodeClient` instance
+    ///  let client = AuthorizationCodeClient::new();
+    ///  let mut clients = BTreeMap::new();
+    ///  clients.insert(
+    ///    "google".to_string(),
+    ///    OAuth2ClientGrantEnum::AuthorizationCode(Arc::new(Mutex::new(authorization_code_client))),
+    ///  );
+    ///  let mut client_store = OAuth2ClientStore::new(clients);
+    ///  let app = Router::new().route("/auth/google_callback", get(google_callback)).layer(Extension(Arc::new(client_store))).layer(session_layer);
+    ///
+    ///  #[derive(Debug, Deserialize)]
+    ///  pub struct AuthRequest {
+    ///     code: String,
+    ///     state: String,
+    ///  }
+    ///  #[derive(Deserialize, Clone, Debug)]
+    ///  pub struct UserProfile {
+    ///     email: String,
+    ///  }
+    ///  pub async fn google_callback(Extension(session_store): Extension<Session>, Extension(oauth_client_store): Extension<Arc<OAuth2ClientStore>>, Query(query): Query<AuthRequest>, jar: PrivateCookieJar) -> String {
+    ///     // Get the previous stored csrf_token from the store
+    ///     let csrf_token = session_store.get::<String>("csrf_token").await.unwrap();
+    ///     // Get the client from the store
+    ///     let client = oauth_client_store.get("google").unwrap();
+    ///     // Get the token and profile from the client   
+    ///     let (token, profile) = client.verify_code_from_callback(query.code, query.state, csrf_token).await.unwrap();
+    ///     // Parse the user's profile
+    ///     let profile = profile.json::<UserProfile>().await.unwrap();
+    ///     let secs: i64 = token.access_token().expires_in().as_secs().try_into().unwrap();
+    ///     // Create a new user based on user's profile into your database
+    ///     // Create a cookie for the user's session
+    ///     let cookie = axum_extra::extract::cookie::Cookie::build(("sid", db_user_id))
+    ///                                 .domain("localhost")
+    ///                                 // only for testing purposes, toggle this to true in production
+    ///                                 .secure(false)
+    ///                                 .http_only(true)
+    ///                                 .max_age(Duration::seconds(secs));
+    ///      // Redirect the user to the protected route
+    ///      let jar = jar.add(cookie);
+    ///      Ok((jar, Redirect::to("/protected")))
+    /// }
+    ///     
+    async fn verify_code_from_callback(
         &mut self,
         code: String,
         state: String,
@@ -125,7 +306,7 @@ pub trait AuthorizationCodeGrantTrait: Send + Sync {
         // Clear outdated flow states
         client.remove_expire_flow();
         // Compare csrf token, use subtle to prevent time attack
-        if !constant_time_compare(&csrf_token, &state) {
+        if !AuthorizationCodeClient::constant_time_compare(&csrf_token, &state) {
             return Err(OAuth2ClientError::CsrfTokenError);
         }
         // Get the pkce_verifier for exchanging code
@@ -163,9 +344,8 @@ impl AuthorizationCodeGrantTrait for AuthorizationCodeClient {
 mod tests {
     use oauth2::url::form_urlencoded;
     use serde::{Deserialize, Serialize};
-    use tracing_subscriber::fmt::format;
     use wiremock::{
-        matchers::{method, path},
+        matchers::{basic_auth, bearer_token, body_string_contains, method, path},
         Mock, MockServer, ResponseTemplate,
     };
 
@@ -174,6 +354,7 @@ mod tests {
     struct Settings {
         client_id: String,
         client_secret: String,
+        code: String,
         auth_url: String,
         token_url: String,
         redirect_url: String,
@@ -183,6 +364,7 @@ mod tests {
         profile_mock_body: UserProfile,
         mock_server: MockServer,
     }
+
     #[derive(Deserialize, Serialize, Clone, Debug)]
     struct ExchangeMockBody {
         access_token: String,
@@ -203,10 +385,6 @@ mod tests {
 
             // Use one of these addresses to configure your client
             let url = server.uri();
-
-            let user_profile = UserProfile {
-                email: "test_email".to_string(),
-            };
             let exchange_mock_body = ExchangeMockBody {
                 access_token: "test_access_token".to_string(),
                 token_type: "bearer".to_string(),
@@ -219,6 +397,7 @@ mod tests {
             Self {
                 client_id: "test_client_id".to_string(),
                 client_secret: "test_client_secret".to_string(),
+                code: "test_code".to_string(),
                 auth_url: format!("{}/auth_url", url),
                 token_url: format!("{}/token_url", url),
                 redirect_url: format!("{}/redirect_url", url),
@@ -229,12 +408,6 @@ mod tests {
                 mock_server: server,
             }
         }
-    }
-
-    fn get_base_url(url: &Url) -> String {
-        let scheme = url.scheme();
-        let host = url.host_str().unwrap_or_default(); // Get the host as a str, default to empty string if not present
-        format!("{}://{}", scheme, host)
     }
 
     fn get_base_url_with_path(url: &Url) -> String {
@@ -252,7 +425,7 @@ mod tests {
         let settings = Settings::new().await;
         let credentials = AuthorizationCodeCredentials {
             client_id: settings.client_id.to_string(),
-            client_secret: Some(settings.client_id.to_string()),
+            client_secret: Some(settings.client_secret.to_string()),
         };
         let config = AuthorizationCodeUrlConfig {
             auth_url: settings.auth_url.to_string(),
@@ -275,6 +448,10 @@ mod tests {
         QueryMapError(String),
         #[error("Unable to deserialize profile")]
         ProfileError,
+        #[error("Mock json data parse Error")]
+        MockJsonDataError(#[from] serde_json::Error),
+        #[error("Mock form data error")]
+        MockFormDataError(#[from] serde_urlencoded::ser::Error),
     }
 
     #[tokio::test]
@@ -329,28 +506,46 @@ mod tests {
     #[tokio::test]
     async fn exchange_code() -> Result<(), TestError> {
         let (mut client, settings) = create_client().await?;
-
+        let token_form_body = vec![
+            serde_urlencoded::to_string([("code", &settings.code)])?,
+            serde_urlencoded::to_string([("redirect_uri", &settings.redirect_url)])?,
+            serde_urlencoded::to_string([("grant_type", "authorization_code")])?,
+        ];
         // Create a mock for the token exchange - https://www.oauth.com/oauth2-servers/access-tokens/authorization-code-request/
-        Mock::given(method("POST"))
+        let mut token_mock = Mock::given(method("POST"))
             .and(path("/token_url"))
-            .respond_with(ResponseTemplate::new(200).set_body_json(settings.exchange_mock_body))
+            // Client Authorization Auth Header from RFC6749(OAuth2) - https://datatracker.ietf.org/doc/html/rfc6749#section-2.3
+            .and(basic_auth(
+                settings.client_id.clone(),
+                settings.client_secret.clone(),
+            ));
+        // Access Token Request Body from RFC6749(OAuth2) - https://datatracker.ietf.org/doc/html/rfc6749#section-4.1.3
+        for url in token_form_body {
+            token_mock = token_mock.and(body_string_contains(url));
+        }
+        token_mock
+            .respond_with(
+                ResponseTemplate::new(200).set_body_json(settings.exchange_mock_body.clone()),
+            )
             .expect(1)
             .mount(&settings.mock_server)
             .await;
         // Create a mock for getting profile - https://www.oauth.com/oauth2-servers/access-tokens/access-token-response/
-
         Mock::given(method("GET"))
             .and(path("/profile_url"))
+            .and(bearer_token(
+                settings.exchange_mock_body.access_token.clone(),
+            ))
             .respond_with(ResponseTemplate::new(200).set_body_json(settings.profile_mock_body))
             .expect(1)
             .mount(&settings.mock_server)
             .await;
-        let (url, csrf_token) = client.get_authorization_url();
-        let code = "test_code".to_string();
+        let (_url, csrf_token) = client.get_authorization_url();
+
         let state = csrf_token.secret().to_string();
         let csrf_token = csrf_token.secret().to_string();
-        let (token, profile) = client
-            .verify_user_with_code(code, state, csrf_token)
+        let (_token, profile) = client
+            .verify_code_from_callback(settings.code, state, csrf_token)
             .await?;
 
         // Parse the user's profile
