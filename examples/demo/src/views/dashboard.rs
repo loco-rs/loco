c2b3ca006db5fd850b3b4c1ed20c39dd17087286
Dotan J. Nahum <jondotan@gmail.com>
2024-02-11 15:03:42 +0200

    Update dashboard.rs

--- examples/demo/src/views/dashboard.rs
+++ examples/demo/src/views/dashboard.rs
@@ -2,5 +2,5 @@ use loco_rs::prelude::*;
 use serde_json::json;
 
 pub fn home(v: impl ViewRenderer) -> Result<impl IntoResponse> {
-    format::render().view(&v, "home/hello.html", json!(()))
+    format::render().view(&v, "home/hello.html", json!({}))
 }
