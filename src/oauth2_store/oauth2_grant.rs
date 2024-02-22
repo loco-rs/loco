ed5ba0561f081fb6aa0c2ab3f09a8a953fb07eae
Naiker <yinho999@gmail.com>
2024-02-22 22:41:44 +0000

    fmt checks

--- src/oauth2_store/oauth2_grant.rs
+++ src/oauth2_store/oauth2_grant.rs
@@ -1,8 +1,8 @@
 use std::sync::Arc;
 
 use tokio::sync::Mutex;
-use crate::oauth2_store::grants::authorization_code::AuthorizationCodeGrantTrait;
 
+use crate::oauth2_store::grants::authorization_code::AuthorizationCodeGrantTrait;
 
 #[derive(Clone)]
 pub enum OAuth2ClientGrantEnum {
