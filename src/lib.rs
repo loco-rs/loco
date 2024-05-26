3cf52a8a8ff90f64bd596c96cb6ef732092aa778
Elad Kaplan <kaplan.elad@gmail.com>
2024-05-26 12:42:00 +0300

    validate ci

--- src/lib.rs
+++ src/lib.rs
@@ -26,7 +26,7 @@
 //! $ cargo install sea-orm-cli # Only when DB is needed
 //! ```
 //!
-//! Now you can create your new app (choose "SaaS app").
+//! Now you can create your new app (choose "`SaaS` app").
 //!
 //! ```sh
 //! $ loco new
@@ -95,7 +95,7 @@
 //!
 //! ## Project Status
 //! + Stateless APIs
-//! + Complete SaaS products with user authentication
+//! + Complete `SaaS` products with user authentication
 //! + Purpose-built services such as ML inference endpoints
 //! + Full stack projects with separate frontend project integrated with Loco
 //! + Hobby projects full-stack with backend and HTML frontend
