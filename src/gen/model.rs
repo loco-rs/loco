802b79232041af20b50de6a8cf428187ea06a6a7
Dotan J. Nahum <jondotan@gmail.com>
2024-09-01 14:53:29 +0300

    CI win32 wip (#724)
    prepare for win32 ci incl. migrations

--- src/gen/model.rs
+++ src/gen/model.rs
@@ -1,4 +1,4 @@
-use std::env::current_dir;
+use std::{collections::HashMap, env::current_dir};
 
 use chrono::Utc;
 use duct::cmd;
@@ -60,9 +60,12 @@ pub fn generate<H: Hooks>(
 
     if !migration_only {
         let cwd = current_dir()?;
+        let env_map: HashMap<_, _> = std::env::vars().collect();
+
         let _ = cmd!("cargo", "loco", "db", "migrate",)
             .stderr_to_stdout()
             .dir(cwd.as_path())
+            .full_env(&env_map)
             .run()
             .map_err(|err| {
                 Error::Message(format!(
@@ -72,6 +75,7 @@ pub fn generate<H: Hooks>(
         let _ = cmd!("cargo", "loco", "db", "entities",)
             .stderr_to_stdout()
             .dir(cwd.as_path())
+            .full_env(&env_map)
             .run()
             .map_err(|err| {
                 Error::Message(format!(
