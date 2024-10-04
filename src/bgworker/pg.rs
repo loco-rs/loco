79440eaf3eca8e2147c615363c7da06c2d0abd7e
Dotan Nahum <jondotan@gmail.com>
2024-10-04 13:43:26 +0300

    first implementation of multi worker backend

---
+++ src/bgworker/pg.rs
@@ -0,0 +1,313 @@
+/// Postgres based background job queue provider
+use std::{collections::HashMap, future::Future, pin::Pin, sync::Arc, time::Duration};
+
+use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
+use serde_json::Value as JsonValue;
+use sqlx::{
+    postgres::{PgConnectOptions, PgPoolOptions},
+    ConnectOptions, Postgres,
+};
+use tokio::{task::JoinHandle, time::sleep};
+use tracing::{debug, error, trace};
+use ulid::Ulid;
+
+use super::{BackgroundWorker, Queue};
+use crate::{config::PostgresQueueConfig, Result};
+
+/// Postgres connection pool
+pub type Pool = sqlx::Pool<Postgres>;
+
+type TaskId = String;
+type TaskData = JsonValue;
+type TaskStatus = String;
+
+type TaskHandler = Box<
+    dyn Fn(
+            TaskId,
+            TaskData,
+        ) -> Pin<Box<dyn std::future::Future<Output = Result<(), crate::Error>> + Send>>
+        + Send
+        + Sync,
+>;
+
+#[derive(Debug, Deserialize, Serialize, sqlx::FromRow)]
+struct Task {
+    pub id: TaskId,
+    pub name: String,
+    #[allow(clippy::struct_field_names)]
+    pub task_data: TaskData,
+    pub status: TaskStatus,
+    pub run_at: DateTime<Utc>,
+    pub interval: Option<i64>,
+}
+
+pub struct TaskRegistry {
+    handlers: Arc<HashMap<String, TaskHandler>>,
+}
+
+impl TaskRegistry {
+    /// Creates a new `TaskRegistry`.
+    pub fn new() -> Self {
+        Self {
+            handlers: Arc::new(HashMap::new()),
+        }
+    }
+
+    /// Registers a task handler with the provided name.
+    pub fn register_worker<Args, W>(&mut self, name: String, worker: W)
+    where
+        Args: Send + Serialize + Sync + 'static,
+        W: BackgroundWorker<Args> + 'static,
+        for<'de> Args: Deserialize<'de>,
+    {
+        let worker = Arc::new(worker);
+        let wrapped_handler = move |_task_id: String, task_data: TaskData| {
+            let w = worker.clone();
+
+            Box::pin(async move {
+                let args = serde_json::from_value::<Args>(task_data);
+                match args {
+                    Ok(args) => w.perform(args).await,
+                    Err(err) => Err(err.into()),
+                }
+            }) as Pin<Box<dyn Future<Output = Result<(), crate::Error>> + Send>>
+        };
+
+        Arc::get_mut(&mut self.handlers)
+            .unwrap()
+            .insert(name, Box::new(wrapped_handler));
+    }
+
+    /// Returns a reference to the task handlers.
+    pub fn handlers(&self) -> &Arc<HashMap<String, TaskHandler>> {
+        &self.handlers
+    }
+
+    /// Runs the task handlers with the provided number of workers.
+    pub fn run(&self, pool: &Pool, opts: &RunOpts) -> Vec<JoinHandle<()>> {
+        let mut tasks = Vec::new();
+
+        let interval = opts.poll_interval_sec;
+        for idx in 0..opts.num_workers {
+            let handlers = self.handlers.clone();
+
+            let pool = pool.clone();
+            let task = tokio::spawn(async move {
+                loop {
+                    trace!(
+                        pool_conns = pool.num_idle(),
+                        worker_num = idx,
+                        "pg workers stats"
+                    );
+                    let task_opt = match dequeue(&pool).await {
+                        Ok(t) => t,
+                        Err(err) => {
+                            error!(err = err.to_string(), "cannot fetch from queue");
+                            None
+                        }
+                    };
+
+                    if let Some(task) = task_opt {
+                        debug!(task_id = task.id, name = task.name, "working on task");
+                        if let Some(handler) = handlers.get(&task.name) {
+                            match handler(task.id.clone(), task.task_data.clone()).await {
+                                Ok(()) => {
+                                    if let Err(err) =
+                                        complete_task(&pool, &task.id, task.interval).await
+                                    {
+                                        error!(
+                                            err = err.to_string(),
+                                            task = ?task,
+                                            "cannot complete task"
+                                        );
+                                    }
+                                }
+                                Err(err) => {
+                                    if let Err(err) = fail_task(&pool, &task.id, &err).await {
+                                        error!(
+                                            err = err.to_string(),
+                                            task = ?task,
+                                            "cannot fail task"
+                                        );
+                                    }
+                                }
+                            }
+                        } else {
+                            error!(task = task.name, "no handler found for task");
+                        }
+                    } else {
+                        sleep(Duration::from_secs(interval.into())).await;
+                    }
+                }
+            });
+
+            tasks.push(task);
+        }
+
+        tasks
+    }
+}
+
+impl Default for TaskRegistry {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+async fn connect(cfg: &PostgresQueueConfig) -> Result<Pool> {
+    let mut conn_opts: PgConnectOptions = cfg.uri.parse()?;
+    if !cfg.enable_logging {
+        conn_opts = conn_opts.disable_statement_logging();
+    }
+    let pool = PgPoolOptions::new()
+        .min_connections(cfg.min_connections)
+        .max_connections(cfg.max_connections)
+        .idle_timeout(Duration::from_millis(cfg.idle_timeout))
+        .acquire_timeout(Duration::from_millis(cfg.connect_timeout))
+        .connect_with(conn_opts)
+        .await?;
+    Ok(pool)
+}
+
+pub async fn initialize_database(pool: &Pool) -> Result<()> {
+    debug!("pg worker: initialize database");
+    sqlx::raw_sql(
+        r"
+            CREATE TABLE IF NOT EXISTS pg_loco_queue (
+                id VARCHAR NOT NULL,
+                name VARCHAR NOT NULL,
+                task_data JSONB NOT NULL,
+                status VARCHAR NOT NULL DEFAULT 'queued',
+                run_at TIMESTAMPTZ NOT NULL,
+                interval BIGINT,
+                created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+                updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+            );
+            ",
+    )
+    .execute(pool)
+    .await?;
+    Ok(())
+}
+
+pub async fn enqueue(
+    pool: &Pool,
+    name: &str,
+    task_data: TaskData,
+    run_at: DateTime<Utc>,
+    interval: Option<Duration>,
+) -> Result<TaskId> {
+    let task_data_json = serde_json::to_value(task_data)?;
+
+    #[allow(clippy::cast_possible_truncation)]
+    let interval_ms: Option<i64> = interval.map(|i| i.as_millis() as i64);
+
+    let id = Ulid::new().to_string();
+    sqlx::query(
+        "INSERT INTO pg_loco_queue (id, task_data, name, run_at, interval) VALUES ($1, $2, $3, \
+         $4, $5)",
+    )
+    .bind(id.clone())
+    .bind(task_data_json)
+    .bind(name)
+    .bind(run_at)
+    .bind(interval_ms)
+    .execute(pool)
+    .await?;
+    Ok(id)
+}
+
+async fn dequeue(client: &Pool) -> Result<Option<Task>> {
+    let mut tx = client.begin().await?;
+    let row = sqlx::query_as::<_, Task>(
+        "SELECT id, name, task_data, status, run_at, interval FROM pg_loco_queue WHERE status = \
+         'queued' AND run_at <= NOW() ORDER BY run_at LIMIT 1 FOR UPDATE SKIP LOCKED",
+    )
+    .fetch_optional(&mut *tx)
+    .await?;
+
+    if let Some(task) = row {
+        sqlx::query(
+            "UPDATE pg_loco_queue SET status = 'processing', updated_at = NOW() WHERE id = $1",
+        )
+        .bind(&task.id)
+        .execute(&mut *tx)
+        .await?;
+
+        tx.commit().await?;
+
+        Ok(Some(task))
+    } else {
+        Ok(None)
+    }
+}
+
+async fn complete_task(pool: &Pool, task_id: &TaskId, interval_ms: Option<i64>) -> Result<()> {
+    if let Some(interval_ms) = interval_ms {
+        let next_run_at = Utc::now() + chrono::Duration::milliseconds(interval_ms);
+        sqlx::query(
+            "UPDATE pg_loco_queue SET status = 'queued', updated_at = NOW(), run_at = $1 WHERE id \
+             = $2",
+        )
+        .bind(next_run_at)
+        .bind(task_id)
+        .execute(pool)
+        .await?;
+    } else {
+        sqlx::query(
+            "UPDATE pg_loco_queue SET status = 'completed', updated_at = NOW() WHERE id = $1",
+        )
+        .bind(task_id)
+        .execute(pool)
+        .await?;
+    }
+    Ok(())
+}
+
+async fn fail_task(pool: &Pool, task_id: &TaskId, error: &crate::Error) -> Result<()> {
+    let msg = error.to_string();
+    error!(err = msg, "failed task");
+    let error_json = serde_json::json!({ "error": msg });
+    sqlx::query(
+        "UPDATE pg_loco_queue SET status = 'failed', updated_at = NOW(), task_data = task_data || \
+         $1::jsonb WHERE id = $2",
+    )
+    .bind(error_json)
+    .bind(task_id)
+    .execute(pool)
+    .await?;
+    Ok(())
+}
+
+pub async fn clear(pool: &Pool) -> Result<()> {
+    sqlx::query("DELETE from pg_loco_queue")
+        .execute(pool)
+        .await?;
+    Ok(())
+}
+
+pub async fn ping(pool: &Pool) -> Result<()> {
+    sqlx::query("SELECT id from pg_loco_queue LIMIT 1")
+        .execute(pool)
+        .await?;
+    Ok(())
+}
+
+pub struct RunOpts {
+    pub num_workers: u32,
+    pub poll_interval_sec: u32,
+}
+
+pub async fn create_provider(qcfg: &PostgresQueueConfig) -> Result<Queue> {
+    let pool = connect(qcfg).await.map_err(Box::from)?;
+    let registry = TaskRegistry::new();
+    Ok(Queue::Postgres(
+        pool,
+        Arc::new(tokio::sync::Mutex::new(registry)),
+        RunOpts {
+            num_workers: qcfg.num_workers,
+            poll_interval_sec: qcfg.poll_interval_sec,
+        },
+    ))
+}
---
+++ src/bgworker/skq.rs
@@ -0,0 +1,123 @@
+use std::{marker::PhantomData, sync::Arc};
+
+use async_trait::async_trait;
+use bb8::Pool;
+use sidekiq::{Processor, RedisConnectionManager};
+
+use super::{BackgroundWorker, Queue};
+use crate::{config::RedisQueueConfig, Result};
+pub type RedisPool = Pool<RedisConnectionManager>;
+
+pub struct SidekiqBackgroundWorker<W, A> {
+    pub inner: W, // Now we store the worker with its actual type instead of a trait object
+    _phantom: PhantomData<A>,
+}
+impl<W, A> SidekiqBackgroundWorker<W, A>
+where
+    W: BackgroundWorker<A> + 'static,
+    A: Send + Sync + serde::Serialize + 'static,
+{
+    pub fn new(worker: W) -> Self {
+        Self {
+            inner: worker,
+            _phantom: PhantomData, // Initialize PhantomData for A
+        }
+    }
+}
+
+#[async_trait]
+impl<W, A> sidekiq::Worker<A> for SidekiqBackgroundWorker<W, A>
+where
+    W: BackgroundWorker<A> + Send + Sync + 'static,
+    A: Send + Sync + serde::Serialize + 'static,
+{
+    fn class_name() -> String
+    where
+        Self: Sized,
+    {
+        // Now we can use the worker's static class_name method
+        W::class_name()
+    }
+
+    async fn perform(&self, args: A) -> sidekiq::Result<()> {
+        // Forward the perform call to the inner worker
+        let res = self.inner.perform(args).await;
+        res.map_err(|e| sidekiq::Error::Any(Box::from(e)))
+    }
+}
+pub async fn clear(pool: &RedisPool) -> Result<()> {
+    let mut conn = pool.get().await?;
+    sidekiq::redis_rs::cmd("FLUSHDB")
+        .query_async::<_, ()>(conn.unnamespaced_borrow_mut())
+        .await?;
+    Ok(())
+}
+
+pub async fn enqueue(
+    pool: &RedisPool,
+    class: String,
+    queue: Option<String>,
+    args: impl serde::Serialize + Send,
+) -> Result<()> {
+    sidekiq::opts()
+        .queue(queue.unwrap_or_else(|| "default".to_string()))
+        .perform_async(pool, class, args)
+        .await
+        .map_err(Box::from)?;
+    Ok(())
+}
+
+pub async fn ping(pool: &RedisPool) -> Result<()> {
+    let mut conn = pool.get().await?;
+    Ok(sidekiq::redis_rs::cmd("PING")
+        .query_async::<_, ()>(conn.unnamespaced_borrow_mut())
+        .await?)
+}
+
+pub const DEFAULT_QUEUES: &[&str] = &["default", "mailer"];
+
+pub fn get_queues(config_queues: &Option<Vec<String>>) -> Vec<String> {
+    let mut queues = DEFAULT_QUEUES
+        .iter()
+        .map(ToString::to_string)
+        .collect::<Vec<_>>();
+
+    // add if missing
+    if let Some(config_queues) = config_queues {
+        for q in config_queues {
+            if !queues.iter().any(|aq| q == aq) {
+                queues.push(q.to_string());
+            }
+        }
+    }
+
+    queues
+}
+pub async fn create_provider(qcfg: &RedisQueueConfig) -> Result<Queue> {
+    let manager = RedisConnectionManager::new(qcfg.uri.clone())?;
+    let redis = Pool::builder().build(manager).await?;
+    let queues = get_queues(&qcfg.queues);
+    Ok(Queue::Redis(
+        redis.clone(),
+        Arc::new(tokio::sync::Mutex::new(Processor::new(redis, queues))),
+    ))
+}
+
+#[cfg(test)]
+mod tests {
+    use insta::assert_debug_snapshot;
+
+    use super::get_queues;
+
+    #[test]
+    fn test_default_custom_queues() {
+        let default_queues = get_queues(&None);
+        assert_debug_snapshot!(default_queues);
+
+        let default_queues2 = get_queues(&Some(vec![]));
+        assert_debug_snapshot!(default_queues2);
+
+        let merged_queues = get_queues(&Some(vec!["foo".to_string(), "bar".to_string()]));
+        assert_debug_snapshot!(merged_queues);
+    }
+}
