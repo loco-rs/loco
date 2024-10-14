97143cb096211fabb43dcd67887f41fee6ebe78f
Dotan Nahum <jondotan@gmail.com>
2024-10-14 15:13:15 +0300

    rest-api: clean up

--- starters/rest-api/tests/requests/auth.rs
+++ starters/rest-api/tests/requests/auth.rs
@@ -193,3 +193,27 @@ async fn can_reset_password() {
     })
     .await;
 }
+
+
+#[tokio::test]
+#[serial]
+async fn can_get_current_user() {
+    configure_insta!();
+
+    testing::request::<App, _, _>(|request, ctx| async move {
+        let user = prepare_data::init_user_login(&request, &ctx).await;
+
+        let (auth_key, auth_value) = prepare_data::auth_header(&user.token);
+        let response = request
+            .get("/api/user/current")
+            .add_header(auth_key, auth_value)
+            .await;
+
+        with_settings!({
+            filters => testing::cleanup_user_model()
+        }, {
+            assert_debug_snapshot!((response.status_code(), response.text()));
+        });
+    })
+    .await;
+}
