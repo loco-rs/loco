7bb5e4c79ec0ab323c64a45128a37d8df9ebceb6
Dotan Nahum <jondotan@gmail.com>
2024-10-28 11:17:43 +0200

    fix: injection points

--- starters/saas/migration/src/lib.rs
+++ starters/saas/migration/src/lib.rs
@@ -9,6 +9,8 @@ pub struct Migrator;
 #[async_trait::async_trait]
 impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
-        vec![Box::new(m20220101_000001_users::Migration)]
+        vec![
+            // inject-below
+            Box::new(m20220101_000001_users::Migration)]
     }
 }
