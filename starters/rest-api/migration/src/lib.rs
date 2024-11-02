42c322dc3c13af3c570c7123f0debbefb67dd413
Arthur Bruel <arthur_bruel@hotmail.com>
2024-11-02 16:03:25 -0300

    fix: codegen generating migrations that apply backwards
    BREAKING CHANGE: requires inject comment in `migration/src/lib.rs` to now be `inject-above`

--- starters/rest-api/migration/src/lib.rs
+++ starters/rest-api/migration/src/lib.rs
@@ -10,8 +10,8 @@ pub struct Migrator;
 impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
         vec![
-            // inject-below
             Box::new(m20220101_000001_users::Migration),
+            // inject-above
         ]
     }
 }
