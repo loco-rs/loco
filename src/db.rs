828cd52c9c202013c53dc7cdbaf098848842e0cf
U-Zyn Chua <chua@uzyn.com>
2024-11-09 17:39:13 +0800

    Update updated_at with the current timestamp upon saving (#962)
    * Include updated_at upon updating of a record.
    
    ---------
    
    Co-authored-by: Elad Kaplan <kaplan.elad@gmail.com>
    Co-authored-by: Dotan J. Nahum <jondotan@gmail.com>

--- src/db.rs
+++ src/db.rs
@@ -341,8 +341,22 @@ fn fix_entities() -> AppResult<()> {
 use super::_entities::{module}::{{ActiveModel, Entity}};
 pub type {module_pascal} = Entity;
 
+#[async_trait::async_trait]
 impl ActiveModelBehavior for ActiveModel {{
     // extend activemodel below (keep comment for generators)
+
+    async fn before_save<C>(self, _db: &C, insert: bool) -> std::result::Result<Self, DbErr>
+    where
+        C: ConnectionTrait,
+    {{
+        if !insert && self.updated_at.is_unchanged() {{
+            let mut this = self;
+            this.updated_at = sea_orm::ActiveValue::Set(chrono::Utc::now().into());
+            Ok(this)
+        }} else {{
+            Ok(self)
+        }}
+    }}
 }}
 "
                 ),
