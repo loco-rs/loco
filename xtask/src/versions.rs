564915d144f131bba685fac3f4b6d10f06b9fc75
Dotan J. Nahum <jondotan@gmail.com>
2024-11-14 15:20:21 +0200

    xtask: align bump

--- xtask/src/versions.rs
+++ xtask/src/versions.rs
@@ -38,49 +38,6 @@ fn bump_version_in_file(
 }
 
 pub fn bump_version(version: &str) -> Result<()> {
-    let starters = [
-        "starters/saas/Cargo.toml",
-        "starters/rest-api/Cargo.toml",
-        "starters/lightweight-service/Cargo.toml",
-    ];
-
-    // turn starters to local "../../" version for testing
-    for cargo in starters {
-        bump_version_in_file(
-            cargo,
-            // loco-rs = { version =".."
-            r#"loco-rs\s*=\s*\{\s*version\s*=\s*"[^"]+""#,
-            r#"loco-rs = { path="../../""#,
-            false,
-        );
-    }
-
-    println!("Testing starters CI");
-    let starter_projects: Vec<ci::RunResults> = ci::run_all_in_folder(Path::new("starters"))?;
-
-    println!("Starters CI results:");
-    println!("{}", out::print_ci_results(&starter_projects));
-    for starter in &starter_projects {
-        if !starter.is_valid() {
-            return Err(Error::Message(format!(
-                "starter {} ins not passing the CI",
-                starter.path.display()
-            )));
-        }
-    }
-
-    // all oK
-    // turn starters from local to version
-    for cargo in starters {
-        bump_version_in_file(
-            cargo,
-            // loco-rs = { path =".."
-            r#"loco-rs\s*=\s*\{\s*path\s*=\s*"[^"]+?""#,
-            &format!(r#"loco-rs = {{ version = "{version}""#),
-            false,
-        );
-    }
-
     // replace main versions
     let version_replacement = format!(r#"version = "{version}""#);
     bump_version_in_file("Cargo.toml", r"(?m)^version.*$", &version_replacement, true);
@@ -95,5 +52,16 @@ pub fn bump_version(version: &str) -> Result<()> {
     // sync new version to subcrates in main Cargo.toml
     let loco_gen_dep = format!(r#"loco-gen = {{ version = "{version}","#);
     bump_version_in_file("Cargo.toml", r"(?m)^loco-gen [^,]*,", &loco_gen_dep, false);
+
+    // replace the loco new version pointer
+    // pub const LOCO_VERSION: &str = "0.13";
+    let const_version_replacement = format!(r#"pub const LOCO_VERSION: &str = "{version}";"#);
+    bump_version_in_file(
+        "loco-new/src/lib.rs",
+        r#"(?m)^pub const LOCO_VERSION: &str = "0.13";$"#,
+        &const_version_replacement,
+        true,
+    );
+
     Ok(())
 }
