a29d3eec7a6196ced8650b137b19d3d7520400af
Dotan J. Nahum <jondotan@gmail.com>
2024-11-14 03:57:34 +0200

    ci

--- loco-new/tests/assertion/string.rs
+++ loco-new/tests/assertion/string.rs
@@ -6,7 +6,8 @@ pub fn assert_line_regex(content: &str, expected: &str) {
 
     // Use assert! to check the regex match and panic if it fails
     assert!(
-        re.is_match(content),
+        // sanitize windows crlf
+        re.is_match(&content.replace('\r', "")),
         "Assertion failed: The content did not match the expected string. Expected: '{expected}', \
          content:\n{content}"
     );
