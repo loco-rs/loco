819b45ef54a32dbaf313b297863031aeaafb7733
NexVeridian <nexveridian@gmail.com>
2025-01-21 12:52:07 -0800

    allow and defualt to None jwt_location

--- src/auth/openapi.rs
+++ src/auth/openapi.rs
@@ -7,7 +7,7 @@ use utoipa::{
 
 use crate::{app::AppContext, config::JWTLocation};
 
-static JWT_LOCATION: OnceLock<JWTLocation> = OnceLock::new();
+static JWT_LOCATION: OnceLock<Option<JWTLocation>> = OnceLock::new();
 
 pub fn get_jwt_location_from_ctx(ctx: &AppContext) -> JWTLocation {
     ctx.config
@@ -19,16 +19,16 @@ pub fn get_jwt_location_from_ctx(ctx: &AppContext) -> JWTLocation {
         .clone()
 }
 
-pub fn set_jwt_location_ctx(ctx: &AppContext) -> &'static JWTLocation {
+pub fn set_jwt_location_ctx(ctx: &AppContext) -> &'static Option<JWTLocation> {
     set_jwt_location(get_jwt_location_from_ctx(ctx))
 }
 
-pub fn set_jwt_location(jwt_location: JWTLocation) -> &'static JWTLocation {
-    JWT_LOCATION.get_or_init(|| jwt_location)
+pub fn set_jwt_location(jwt_location: JWTLocation) -> &'static Option<JWTLocation> {
+    JWT_LOCATION.get_or_init(|| Some(jwt_location))
 }
 
-fn get_jwt_location() -> &'static JWTLocation {
-    JWT_LOCATION.get().unwrap_or(&JWTLocation::Bearer)
+fn get_jwt_location() -> &'static Option<JWTLocation> {
+    JWT_LOCATION.get().unwrap_or(&None)
 }
 
 pub struct SecurityAddon;
@@ -36,30 +36,32 @@ pub struct SecurityAddon;
 /// Adds security to the OpenAPI doc, using the JWT location in the config
 impl Modify for SecurityAddon {
     fn modify(&self, openapi: &mut utoipa::openapi::OpenApi) {
-        if let Some(components) = openapi.components.as_mut() {
-            components.add_security_schemes_from_iter([
-                (
-                    "jwt_token",
-                    match get_jwt_location() {
-                        JWTLocation::Bearer => SecurityScheme::Http(
-                            HttpBuilder::new()
-                                .scheme(HttpAuthScheme::Bearer)
-                                .bearer_format("JWT")
-                                .build(),
-                        ),
-                        JWTLocation::Query { name } => {
-                            SecurityScheme::ApiKey(ApiKey::Query(ApiKeyValue::new(name)))
-                        }
-                        JWTLocation::Cookie { name } => {
-                            SecurityScheme::ApiKey(ApiKey::Cookie(ApiKeyValue::new(name)))
-                        }
-                    },
-                ),
-                (
-                    "api_key",
-                    SecurityScheme::ApiKey(ApiKey::Header(ApiKeyValue::new("apikey"))),
-                ),
-            ]);
+        if let Some(jwt_location) = get_jwt_location() {
+            if let Some(components) = openapi.components.as_mut() {
+                components.add_security_schemes_from_iter([
+                    (
+                        "jwt_token",
+                        match jwt_location {
+                            JWTLocation::Bearer => SecurityScheme::Http(
+                                HttpBuilder::new()
+                                    .scheme(HttpAuthScheme::Bearer)
+                                    .bearer_format("JWT")
+                                    .build(),
+                            ),
+                            JWTLocation::Query { name } => {
+                                SecurityScheme::ApiKey(ApiKey::Query(ApiKeyValue::new(name)))
+                            }
+                            JWTLocation::Cookie { name } => {
+                                SecurityScheme::ApiKey(ApiKey::Cookie(ApiKeyValue::new(name)))
+                            }
+                        },
+                    ),
+                    (
+                        "api_key",
+                        SecurityScheme::ApiKey(ApiKey::Header(ApiKeyValue::new("apikey"))),
+                    ),
+                ]);
+            }
         }
     }
 }
