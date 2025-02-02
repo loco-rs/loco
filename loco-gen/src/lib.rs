c6fb232048eda52f4098dcf52fd0ae37bf18eed4
Naiker <yinho999@gmail.com>
2025-02-02 00:25:42 +0000

    fix: update Kamal secrets directory path to use dot prefix

--- loco-gen/src/lib.rs
+++ loco-gen/src/lib.rs
@@ -406,14 +406,14 @@ pub fn generate(rrgen: &RRgen, component: Component, appinfo: &AppInfo) -> Resul
                     "background_queue": background_queue
                 });
                 let config_deploy_yml = Path::new("config/deploy.yml");
-                let kamal_secrets = Path::new("kamal/secrets");
+                let kamal_secrets = Path::new(".kamal/secrets");
                 if config_deploy_yml.exists() {
                     tracing::info!("backing up config/deploy.yml to config/_deploy.yml");
                     fs::rename(config_deploy_yml, Path::new("config/_deploy.yml"))?;
                 }
                 if kamal_secrets.exists() {
                     tracing::info!("backing up kamal/secrets to kamal/_secrets");
-                    fs::rename(kamal_secrets, Path::new("kamal/_secrets"))?;
+                    fs::rename(kamal_secrets, Path::new(".kamal/_secrets"))?;
                 }
                 // render the dockerfile template
                 let mut gen_result_docker =
