d413b3a35d9f95175d290ec1e9843db59cbf9101
Elad Kaplan <kaplan.elad@gmail.com>
2025-03-23 15:40:16 +0200

    remove deprecated loco-cli and starters projects (#1310)

--- loco-cli/src/constants.rs
+++
@@ -1,4 +0,0 @@
-pub const OPTION_BACKGROUND_DEFAULT: &str = "mode: BackgroundQueue";
-pub const OPTION_DB_DEFAULT: &str = "postgres://loco:loco@localhost:5432/loco_app";
-pub const SERVER_BLOCK: &str = "server-block";
-pub const CLIENT_BLOCK: &str = "client-block";
