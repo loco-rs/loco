cb186b124d1e9c4747ca14e0e4cb38815f5e03f1
Ryan Dsouza <dsouza.ryan.11@gmail.com>
2025-04-04 10:28:22 +0200

    fix: prevent breaking changes

--- src/config.rs
+++ src/config.rs
@@ -223,8 +223,7 @@ pub struct Database {
 
     // sqlite configuration
     /// set the foreign key pragma to be enabled or disabled
-    #[serde(default)]
-    pub enable_foreign_keys: bool,
+    pub enable_foreign_keys: Option<bool>,
 }
 
 /// Cache configurations for the application
