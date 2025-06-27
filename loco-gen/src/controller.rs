50e93a080d37e5b5471cec051a38c855c20aeda1
NexVeridian <nexveridian@gmail.com>
2025-06-27 14:42:58 -0700

    rename gen -> r#gen

--- loco-gen/src/controller.rs
+++ loco-gen/src/controller.rs
@@ -1,36 +1,42 @@
-use super::{AppInfo, GenerateResults, Result};
-use crate as gen;
+use std::path::Path;
+
 use rrgen::RRgen;
 use serde_json::json;
-use std::path::Path;
+
+use super::{AppInfo, GenerateResults, Result};
+use crate as r#gen;
 
 pub fn generate(
     rrgen: &RRgen,
     name: &str,
     actions: &[String],
-    kind: &gen::ScaffoldKind,
+    kind: &r#gen::ScaffoldKind,
     appinfo: &AppInfo,
 ) -> Result<GenerateResults> {
     let vars = json!({"name": name, "actions": actions, "pkg_name": appinfo.app_name});
     match kind {
-        gen::ScaffoldKind::Api => gen::render_template(rrgen, Path::new("controller/api"), &vars),
-        gen::ScaffoldKind::Html => {
+        r#gen::ScaffoldKind::Api => {
+            r#gen::render_template(rrgen, Path::new("controller/api"), &vars)
+        }
+        r#gen::ScaffoldKind::Html => {
             let mut gen_result =
-                gen::render_template(rrgen, Path::new("controller/html/controller.t"), &vars)?;
+                r#gen::render_template(rrgen, Path::new("controller/html/controller.t"), &vars)?;
             for action in actions {
                 let vars = json!({"name": name, "action": action, "pkg_name": appinfo.app_name});
-                let res = gen::render_template(rrgen, Path::new("controller/html/view.t"), &vars)?;
+                let res =
+                    r#gen::render_template(rrgen, Path::new("controller/html/view.t"), &vars)?;
                 gen_result.rrgen.extend(res.rrgen);
                 gen_result.local_templates.extend(res.local_templates);
             }
             Ok(gen_result)
         }
-        gen::ScaffoldKind::Htmx => {
+        r#gen::ScaffoldKind::Htmx => {
             let mut gen_result =
-                gen::render_template(rrgen, Path::new("controller/htmx/controller.t"), &vars)?;
+                r#gen::render_template(rrgen, Path::new("controller/htmx/controller.t"), &vars)?;
             for action in actions {
                 let vars = json!({"name": name, "action": action, "pkg_name": appinfo.app_name});
-                let res = gen::render_template(rrgen, Path::new("controller/htmx/view.t"), &vars)?;
+                let res =
+                    r#gen::render_template(rrgen, Path::new("controller/htmx/view.t"), &vars)?;
                 gen_result.rrgen.extend(res.rrgen);
                 gen_result.local_templates.extend(res.local_templates);
             }
