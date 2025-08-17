b8d8401fd2886a91922e4c56b184c2d07a5862c8
Elad Kaplan <kaplan.elad@gmail.com>
2025-08-17 13:07:15 +0300

    fix deploment args

--- src/cli.rs
+++ src/cli.rs
@@ -346,7 +346,7 @@ After running the migration, follow these steps to complete the process:
     /// Generate a deployment infrastructure
     Deployment {
         /// The type of deployment to generate
-        #[clap(short, long, value_enum)]
+        #[clap(value_enum)]
         kind: DeploymentKind,
     },
 
