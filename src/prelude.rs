6c93c289d5a70cfcf2f3f01c80dbfbefdc37a9d2
Fabiano Martins <fabiano.paula.martins@gmail.com>
2025-08-24 11:58:18 -0300

    fix: lint errors adjust

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,7 +1,7 @@
 pub use async_trait::async_trait;
 pub use axum::{
     debug_handler,
-    extract::{Form, Path, State, Multipart, Query},
+    extract::{Form, Multipart, Path, Query, State},
     response::{IntoResponse, Response},
     routing::{delete, get, head, options, patch, post, put, trace},
 };
