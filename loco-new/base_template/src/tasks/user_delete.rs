e34372f2ac92454a349f7405c6c680c052f29194
flopetautschnig (floscodes) <mail@floscodes.net>
2025-12-23 13:49:10 +0100

    chore: refactor code and test

--- loco-new/base_template/src/tasks/user_delete.rs
+++ loco-new/base_template/src/tasks/user_delete.rs
@@ -8,37 +8,30 @@ impl Task for UserDelete {
     fn task(&self) -> TaskInfo {
         TaskInfo {
             name: "user:delete".to_string(),
-            detail: "Delete a user by entering email.\nUsage:\ncargo run task user:delete"
+            detail: "Delete a user by entering pid.\nUsage:\ncargo loco run task user:delete"
                 .to_string(),
         }
     }
     async fn run(&self, app_context: &AppContext, vars: &task::Vars) -> Result<()> {
         let input = match vars.cli_arg("pid") {
-            Ok(email) => email,
+            Ok(pid) => pid,
             Err(_) => return Err(Error::string("pid is mandatory")),
         };
+        let force_flag = vars.cli_arg("force");
 
         let user_to_delete = users::Model::find_by_pid(&app_context.db, &input).await?;
 
-        tracing::info!(
-            message = "User to delete",
-            user_email = &user_to_delete.email,
-            user_pid = user_to_delete.pid.to_string(),
-            "delete user via task"
+        println!(
+            "User to delete:\nUsername: {}\nEmail: {}\nPID: {}",
+            user_to_delete.name, user_to_delete.email, user_to_delete.pid
         );
 
-        let mut confirm = String::new();
-
-        // If test ist running simulate confirmation
-        if let Ok(var) = env::var("TEST_CAN_RUN_USER_DELET_BY_PID") {
-            if var == "true".to_string() {
-                confirm.push_str("yes");
-            }
-        } else {
+        if force_flag.is_err() || force_flag.unwrap().trim() != "true" {
             println!(
                 "Are you sure you want to delete the user {}\n({})\nwith pid '{}'?\nType 'yes' and hit enter to confirm",
                 user_to_delete.name, user_to_delete.email, user_to_delete.pid
             );
+            let mut confirm = String::new();
             let stdin = std::io::stdin();
             stdin.read_line(&mut confirm).map_err(|err| {
                 tracing::error!(
@@ -47,11 +40,11 @@ impl Task for UserDelete {
                 );
                 Error::string(&format!("Failed to read confirmation input. err: {err}",))
             })?;
-        }
 
-        if confirm.trim().to_lowercase() != "yes" {
-            println!("⛔ User deletion cancelled - nothing has been deleted!");
-            return Ok(());
+            if confirm.trim().to_lowercase() != "yes" {
+                println!("⛔ User deletion cancelled - nothing has been deleted!");
+                return Ok(());
+            }
         }
 
         let _deleted_user = user_to_delete
--- loco-new/base_template/tests/tasks/user_delete.rs.t
+++ loco-new/base_template/tests/tasks/user_delete.rs.t
@@ -9,7 +9,6 @@ use serial_test::serial;
 #[tokio::test]
 #[serial]
 async fn can_run_user_delete_by_pid() {
-    env::set_var("TEST_CAN_RUN_USER_DELET_BY_PID", "true");
     let boot = boot_test::<App>().await.unwrap();
 
     let user = users::Model::create_with_password(
@@ -25,7 +24,7 @@ async fn can_run_user_delete_by_pid() {
 
     let pid = user.pid;
 
-    let vars = task::Vars::from_cli_args(vec![("pid".to_string(), pid.to_string())]);
+    let vars = task::Vars::from_cli_args(vec![("pid".to_string(), pid.to_string()), ("force".to_string(), "true".to_string())]);
 
     run_task::<App>(&boot.app_context, Some(&"user:delete".to_string()), &vars)
         .await
@@ -33,6 +32,5 @@ async fn can_run_user_delete_by_pid() {
 
     let user = users::Model::find_by_pid(&boot.app_context.db, &pid.to_string()).await;
     assert!(user.is_err());
-    env::remove_var("TEST_CAN_RUN_USER_DELET_BY_PID");
 }
 
