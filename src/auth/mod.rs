edf6e465b618306c2f729133a3fd3793d133dd7b
Anthony Rubick <68485672+AnthonyMichaelTDM@users.noreply.github.com>
2026-03-09 04:53:24 -0500

    feat: expose feature flags to set the crypto provider of jsonwebtoken

--- src/auth/mod.rs
+++ src/auth/mod.rs
@@ -1,2 +1,11 @@
 #[cfg(feature = "auth_jwt")]
 pub mod jwt;
+
+#[cfg(all(
+    any(feature = "jwt_aws_lc_rs", feature = "jwt_rustcrypto"),
+    not(feature = "auth_jwt")
+))]
+compile_error!("jwt_aws_lc_rs and jwt_rustcrypto features require auth_jwt feature");
+
+#[cfg(all(feature = "jwt_aws_lc_rs", feature = "jwt_rustcrypto"))]
+compile_error!("jwt_aws_lc_rs and jwt_rustcrypto features are mutually exclusive");
