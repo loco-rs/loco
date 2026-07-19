a8d6e4fec62111ae4ad4b60b0a26916706f2eedd
david-szabo97 <david.szabo97@gmail.com>
2026-07-19 21:58:39 +0200

    fix(docs): flash of light theme in dark mode

--- docs-site/static/js/main.js
+++ docs-site/static/js/main.js
@@ -5,10 +5,3 @@ document.getElementById('mode').addEventListener('click', () => {
     localStorage.setItem('theme', document.documentElement.classList.contains('dark') ? 'dark' : 'light');
 
 });
-
-// enforce local storage setting but also fallback to user-agent preferences
-if (localStorage.getItem('theme') === 'dark' || (!localStorage.getItem('theme') && window.matchMedia("(prefers-color-scheme: dark)").matches)) {
-
-    document.documentElement.classList.add('dark');
-
-}
